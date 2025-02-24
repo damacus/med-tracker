b66a7b643af50237e4278b05eb4a173386d4fa92
Dan Webb <dan.webb@damacus.io>
2025-02-24 16:29:01 +0000

    Partial implementation of PWA
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

---
+++ public/service-worker.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = 'med-tracker-v1';
+const OFFLINE_URL = '/offline.html';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  OFFLINE_URL,
+  '/manifest.json',
+  '/assets/images/favicon.svg',
+  '/assets/stylesheets/application.css',
+  '/assets/application.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => {
+          return caches.open(CACHE_NAME)
+            .then((cache) => cache.match(OFFLINE_URL));
+        })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
