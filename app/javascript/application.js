81a5847ed7670483ab21394c1270f396093f7db6
Dan Webb <dan.webb@damacus.io>
2025-02-24 16:50:26 +0000

    Update PWA configuration
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,14 @@ Turbo.session.drive = true
 
 // Import all controllers
 import "controllers"
+
+if ('serviceWorker' in navigator) {
+	// Register the service worker
+	navigator.serviceWorker.register('/service-worker.js')
+	  .then(function(registration) {
+		console.log('Service Worker registered with scope:', registration.scope);
+	  })
+	  .catch(function(error) {
+		console.log('Service Worker registration failed:', error);
+	  });
+  }
