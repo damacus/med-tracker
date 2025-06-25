af9a450d7778c715fb974c8b03f42b514edc6edf
Dan Webb <dan.webb@damacus.io>
2025-06-25 13:58:40 +0100

    fix: Move home page
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

---
+++ app/components/home/index_view.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Components::Home::IndexView < Components::Base
+  def view_template
+    div(class: "container") do
+      div(class: "page-header") do
+        h1(class: "page-title") { "Medicine Tracker" }
+        p(class: "page-subtitle") { "Welcome to your personal medicine tracking application." }
+      end
+
+      div(class: "item-grid") do
+        link_to(medicines_path, class: "item-card") do
+          h2(class: "item-card__title") { "Medicines" }
+          p(class: "item-card__detail") { "Manage your medicine inventory" }
+        end
+
+        link_to(people_path, class: "item-card") do
+          h2(class: "item-card__title") { "People" }
+          p(class: "item-card__detail") { "Manage people and their prescriptions" }
+        end
+      end
+    end
+  end
+end
