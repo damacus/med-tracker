caabe5fe2b7563d977a075eb41d0afd1e0e8ad43
Dan Webb <dan.webb@damacus.io>
2026-01-28 10:18:45 +0000

    refactor: remove WebAuthnService and use Rodauth's built-in WebAuthn
    - Delete app/services/web_authn_service.rb (207 lines of redundant code)
    - Delete spec/services/web_authn_service_spec.rb
    - Update passkey_spec.rb to verify Rodauth WebAuthn features instead
    - Rodauth provides secure session-based challenge storage
    - Eliminates insecure Rails.cache challenge storage vulnerability
    - Reduces maintenance burden by using battle-tested implementation
    
    Fixes med-tracker-taqk

--- spec/features/authentication/passkey_spec.rb
+++ spec/features/authentication/passkey_spec.rb
@@ -29,7 +29,6 @@ RSpec.describe 'PASSKEY-001: WebAuthn/Passkey configuration' do
   scenario 'WebAuthn models are defined' do
     expect { AccountWebauthnKey }.not_to raise_error
     expect { AccountWebauthnUserId }.not_to raise_error
-    expect { WebAuthnService }.not_to raise_error
   end
 
   scenario 'Account model has WebAuthn associations' do
@@ -38,26 +37,9 @@ RSpec.describe 'PASSKEY-001: WebAuthn/Passkey configuration' do
     expect(account.respond_to?(:account_webauthn_user_ids)).to be true
   end
 
-  scenario 'WebAuthnService can generate registration options' do
-    account = create(:account)
-    service = WebAuthnService.new(account)
-
-    options = service.registration_options
-
-    expect(options).to be_a(WebAuthn::PublicKeyCredential::CreationOptions)
-    expect(options.rp.name).to eq('MedTracker')
-    expect(options.user.id).to eq(account.id.to_s)
-    expect(options.authenticator_selection[:user_verification]).to eq('required')
-    expect(options.attestation).to eq('direct')
-  end
-
-  scenario 'WebAuthnService can generate authentication options' do
-    account = create(:account)
-    service = WebAuthnService.new(account)
-
-    options = service.authentication_options
-
-    expect(options).to be_a(WebAuthn::PublicKeyCredential::RequestOptions)
-    expect(options.user_verification).to eq('required')
+  scenario 'Rodauth WebAuthn feature is enabled' do
+    # Verify Rodauth has WebAuthn configured
+    expect(RodauthMain.configuration.features).to include(:webauthn)
+    expect(RodauthMain.configuration.features).to include(:webauthn_login)
   end
 end
