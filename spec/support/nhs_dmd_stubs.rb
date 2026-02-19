770cd970da0d3bfcf1d73e5d9b771530affc1169
Dan Webb <dan.webb@damacus.io>
2026-02-19 23:45:04 +0000

    fix: resolve medicine_lookup_spec failures with network stubs

--- spec/support/nhs_dmd_stubs.rb
+++ spec/support/nhs_dmd_stubs.rb
@@ -16,37 +16,44 @@ module NhsDmdStubs
   end
 
   def stub_nhs_dmd_search(query:, results: [], error: nil, status: 200)
-    if error
-      stub_request(:get, %r{#{NhsDmd::Client::BASE_URL}/ValueSet/\$expand})
-        .with(query: hash_including('filter' => query))
-        .to_return(status: status, body: { error: error }.to_json)
-    else
-      # NHS dm+d search fetches both VMP and AMP value sets
-      [NhsDmd::Client::VMP_VALUE_SET, NhsDmd::Client::AMP_VALUE_SET].each do |value_set|
-        stub_request(:get, %r{#{NhsDmd::Client::BASE_URL}/ValueSet/\$expand})
-          .with(query: hash_including('url' => value_set, 'filter' => query))
-          .to_return(
-            status: 200,
-            body: {
-              expansion: {
-                contains: results.select { |r| r[:system] == 'https://dmd.nhs.uk' }.map do |r|
-                  {
-                    code: r[:code],
-                    display: r[:display],
-                    system: r[:system],
-                    extension: [
-                      {
-                        url: 'http://hl7.org/fhir/StructureDefinition/valueset-concept-comments',
-                        valueString: r[:concept_class]
-                      }
-                    ]
-                  }
-                end
-              }
-            }.to_json,
-            headers: { 'Content-Type' => 'application/json' }
-          )
-      end
+    return stub_nhs_dmd_error(query, status, error) if error
+
+    [NhsDmd::Client::VMP_VALUE_SET, NhsDmd::Client::AMP_VALUE_SET].each do |value_set|
+      stub_nhs_dmd_value_set(value_set, query, results)
+    end
+  end
+
+  private
+
+  def stub_nhs_dmd_error(query, status, error)
+    stub_request(:get, %r{#{NhsDmd::Client::BASE_URL}/ValueSet/\$expand})
+      .with(query: hash_including('filter' => query))
+      .to_return(status: status, body: { error: error }.to_json)
+  end
+
+  def stub_nhs_dmd_value_set(value_set, query, results)
+    stub_request(:get, %r{#{NhsDmd::Client::BASE_URL}/ValueSet/\$expand})
+      .with(query: hash_including('url' => value_set, 'filter' => query))
+      .to_return(
+        status: 200,
+        body: { expansion: { contains: map_results_to_fhir(results) } }.to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      )
+  end
+
+  def map_results_to_fhir(results)
+    results.select { |r| r[:system] == 'https://dmd.nhs.uk' }.map do |r|
+      {
+        code: r[:code],
+        display: r[:display],
+        system: r[:system],
+        extension: [
+          {
+            url: 'http://hl7.org/fhir/StructureDefinition/valueset-concept-comments',
+            valueString: r[:concept_class]
+          }
+        ]
+      }
     end
   end
 end
