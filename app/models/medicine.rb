f1ac458a1678cdec82e88130011c84d85b6cb542
Dan Webb <dan.webb@damacus.io>
2026-02-23 20:54:16 +0000

    feat: add sachet as medicine dosage unit (#530)

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class Medicine < ApplicationRecord # :nodoc:
+  DOSAGE_UNITS = %w[tablet mg ml g mcg IU spray drop sachet].freeze
+
   has_many :dosages, dependent: :destroy
   has_many :prescriptions, dependent: :destroy
   has_many :person_medicines, dependent: :destroy
 
   validates :name, presence: true
+  validates :dosage_unit, inclusion: { in: DOSAGE_UNITS }, allow_blank: true
   validates :current_supply, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :reorder_threshold, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
