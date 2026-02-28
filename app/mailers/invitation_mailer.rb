781f15d9ffa309be81d444beae9fe6a1db58be99
Dan Webb <dan.webb@damacus.io>
2026-02-28 20:11:02 +0000

    feat: upgrade InvitationMailer to multipart HTML+text templates
    Removes inline body string from InvitationMailer#invite and replaces
    with proper ERB templates, giving invitations the same branded HTML
    layout as all other app emails. Updates spec to assert multipart output.

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -4,14 +4,8 @@ class InvitationMailer < ApplicationMailer
   def invite
     @invitation = params[:invitation]
     @token = @invitation.token
+    @role = @invitation.role.humanize
 
-    body = <<~TEXT
-      You have been invited to join MedTracker as a #{@invitation.role}.
-
-      Accept your invitation:
-      #{accept_invitation_url(token: @token)}
-    TEXT
-
-    mail(to: @invitation.email, subject: I18n.t('invitation_mailer.subject'), body: body, content_type: 'text/plain')
+    mail(to: @invitation.email, subject: I18n.t('invitation_mailer.subject'))
   end
 end
