fdc521d83f97e547c067df01d8f4d50033546afb
Dan Webb <dan.webb@damacus.io>
2026-03-10 12:14:18 +0000

    fix: align admin users table columns

--- app/components/admin/users/users_table.rb
+++ app/components/admin/users/users_table.rb
@@ -32,7 +32,6 @@ module Components
               render(RubyUI::TableHead.new { render_sortable_header('Email', 'email') })
               render(RubyUI::TableHead.new { render_sortable_header('Role', 'role') })
               render(RubyUI::TableHead.new { 'Activation' })
-              render(RubyUI::TableHead.new { 'Verification' })
               render RubyUI::TableHead.new(class: 'text-right') { 'Actions' }
             end
           end
