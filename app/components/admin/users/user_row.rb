0349ab2e3ff56cbd007105ae676dba2c45dab3fe
Dan Webb <dan.webb@damacus.io>
2026-03-10 12:29:02 +0000

    fix: center align admin users actions column

--- app/components/admin/users/user_row.rb
+++ app/components/admin/users/user_row.rb
@@ -23,8 +23,8 @@ module Components
             render RubyUI::TableCell.new(class: 'capitalize') { user.role }
             render(RubyUI::TableCell.new { render_status_badge })
             render(RubyUI::TableCell.new { render_verification_button })
-            render RubyUI::TableCell.new(class: 'text-right') do
-              div(class: 'flex gap-2 justify-end') do
+            render RubyUI::TableCell.new(class: 'text-center') do
+              div(class: 'flex gap-2 justify-center') do
                 render RubyUI::Link.new(href: "/admin/users/#{user.id}/edit", variant: :outline, size: :sm) { t('admin.users.user_row.edit') }
                 render_activation_button
               end
