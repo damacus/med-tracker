075bd795c966dc640bfdc91732902a3a1595f70e
Dan Webb <dan.webb@damacus.io>
2026-03-12 10:33:04 +0000

    feat: add assets rebuild tasks for dev and test
    - add dev:assets-rebuild and test:assets-rebuild via internal:run
    
    - precompile assets inside the matching web container
    
    - add taskfile spec covering both definitions

---
+++ spec/tasks/assets_rebuild_taskfiles_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'assets rebuild taskfiles' do
+  def task_config(path)
+    YAML.safe_load(Rails.root.join(path).read)
+  end
+
+  it 'defines dev:assets-rebuild via internal:run' do
+    task = task_config('Taskfiles/dev.yml').dig('tasks', 'assets-rebuild')
+
+    expect(task['cmds']).to eq([
+      {
+        'task' => ':internal:run',
+        'vars' => {
+          'ENVIRONMENT' => 'dev',
+          'COMMAND' => 'rails assets:clobber assets:precompile'
+        }
+      }
+    ])
+  end
+
+  it 'defines test:assets-rebuild via internal:run' do
+    task = task_config('Taskfiles/test.yml').dig('tasks', 'assets-rebuild')
+
+    expect(task['cmds']).to eq([
+      {
+        'task' => ':internal:run',
+        'vars' => {
+          'ENVIRONMENT' => 'test',
+          'COMMAND' => 'rails assets:clobber assets:precompile'
+        }
+      }
+    ])
+  end
+end
