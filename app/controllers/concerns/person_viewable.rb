23be25168a5810f2f28bede3cb2a9ad3c9ecc246
Dan Webb <dan.webb@damacus.io>
2026-04-13 08:48:17 +0100

    refactor: extract person show query

--- app/controllers/concerns/person_viewable.rb
+++ app/controllers/concerns/person_viewable.rb
@@ -6,28 +6,13 @@ module PersonViewable
   private
 
   def person_show_view(person)
-    schedules = person.schedules.includes(:medication, :dosage)
-    person_medications = person.person_medications.includes(:medication).ordered
-    today_start = Time.current.beginning_of_day
-
-    takes_by_schedule = MedicationTake
-                        .where(schedule_id: schedules.map(&:id), taken_at: today_start..)
-                        .order(taken_at: :desc)
-                        .group_by(&:schedule_id)
-
-    takes_by_person_medication = MedicationTake
-                                 .where(person_medication_id: person_medications.map(&:id), taken_at: today_start..)
-                                 .order(taken_at: :desc)
-                                 .group_by(&:person_medication_id)
+    show_data = PersonShowQuery.new(person: person).call
 
     Components::People::ShowView.new(
       person: person,
-      schedules: schedules,
-      person_medications: person_medications,
-      preloaded_takes: {
-        schedules: takes_by_schedule,
-        person_medications: takes_by_person_medication
-      },
+      schedules: show_data.schedules,
+      person_medications: show_data.person_medications,
+      preloaded_takes: show_data.preloaded_takes,
       current_user: current_user
     )
   end
---
+++ spec/services/person_show_query_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PersonShowQuery do
+  describe '#call' do
+    it 'returns the person show read model for one person with only today takes grouped by source' do
+      person = create(:person)
+      other_person = create(:person)
+      show_data = create_show_data_for(person: person, other_person: other_person)
+
+      result = described_class.new(person: person).call
+
+      expect(result.person).to eq(person)
+      expect(result.schedules).to contain_exactly(show_data[:schedule])
+      expect(result.person_medications).to contain_exactly(show_data[:person_medication])
+      expect(result.preloaded_takes[:schedules]).to eq(
+        show_data[:schedule].id => [show_data[:todays_schedule_take]]
+      )
+      expect(result.preloaded_takes[:person_medications]).to eq(
+        show_data[:person_medication].id => [show_data[:todays_person_medication_take]]
+      )
+    end
+  end
+
+  def create_show_data_for(person:, other_person:)
+    schedule = create(:schedule, person: person)
+    other_schedule = create(:schedule, person: other_person)
+    person_medication = create(:person_medication, person: person)
+    other_person_medication = create(:person_medication, person: other_person)
+
+    todays_schedule_take = create_schedule_takes(schedule: schedule, other_schedule: other_schedule)
+    todays_person_medication_take = create_person_medication_takes(
+      person_medication: person_medication,
+      other_person_medication: other_person_medication
+    )
+
+    {
+      schedule: schedule,
+      person_medication: person_medication,
+      todays_schedule_take: todays_schedule_take,
+      todays_person_medication_take: todays_person_medication_take
+    }
+  end
+
+  def create_schedule_takes(schedule:, other_schedule:)
+    todays_schedule_take = create(:medication_take, :for_schedule, :today, schedule: schedule)
+    create(:medication_take, :for_schedule, taken_at: 2.days.ago, schedule: schedule)
+    create(:medication_take, :for_schedule, :today, schedule: other_schedule)
+    todays_schedule_take
+  end
+
+  def create_person_medication_takes(person_medication:, other_person_medication:)
+    todays_person_medication_take = create(
+      :medication_take,
+      :for_person_medication,
+      :today,
+      person_medication: person_medication
+    )
+    create(:medication_take, :for_person_medication, taken_at: 2.days.ago, person_medication: person_medication)
+    create(:medication_take, :for_person_medication, :today, person_medication: other_person_medication)
+    todays_person_medication_take
+  end
+end
