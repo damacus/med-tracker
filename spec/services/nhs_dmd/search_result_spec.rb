3b81395b7072aa875a5557f43679a68483cbadf2
Dan Webb <dan.webb@damacus.io>
2026-07-02 22:53:12 +0100

    feat(lookup): show external medicine details (#1462)

--- spec/services/nhs_dmd/search_result_spec.rb
+++ spec/services/nhs_dmd/search_result_spec.rb
@@ -4,6 +4,17 @@ require 'rails_helper'
 
 RSpec.describe NhsDmd::SearchResult do
   describe '#to_h' do
+    it 'includes optional medicine detail fields' do
+      result = described_class.new(
+        code: '39720311000001101',
+        display: 'Aspirin 300mg tablets',
+        system: 'https://dmd.nhs.uk',
+        **medicine_detail_fields
+      )
+
+      expect(result.to_h).to include(medicine_detail_fields)
+    end
+
     it 'includes a valid HTTPS PIL URL' do
       result = described_class.new(
         code: '39720311000001101',
@@ -33,5 +44,16 @@ RSpec.describe NhsDmd::SearchResult do
         end
       ).to all(be_nil)
     end
+  end
+
+  def medicine_detail_fields
+    {
+      name: 'Aspirin 300mg tablets',
+      description: 'Pain relief medicine',
+      directions: 'Take with water',
+      warnings: 'Do not exceed the stated dose',
+      category: 'Analgesic',
+      package_size: '32 tablets'
+    }
   end
 end
