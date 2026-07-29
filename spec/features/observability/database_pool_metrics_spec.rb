278d0002c662653093b2dbb3beef58b3edfd6145
Dan Webb <dan.webb@damacus.io>
2026-07-29 13:49:32 +0100

    test(observability): satisfy RuboCop

--- spec/features/observability/database_pool_metrics_spec.rb
+++ spec/features/observability/database_pool_metrics_spec.rb
@@ -22,60 +22,72 @@ RSpec.describe 'Database pool metrics export pipeline' do
     meter.collect
 
     observations = meter.gauges.transform_values(&:recordings)
-    expect(observations).to include(
-      'medtracker.db.connection_pool.size' => [[pool.stat.fetch(:size), a_hash_including('db.pool.name', 'db.namespace')]],
-      'medtracker.db.connection_pool.in_use' => [[pool.stat.fetch(:busy), a_hash_including('db.pool.name', 'db.namespace')]],
-      'medtracker.db.connection_pool.idle' => [[pool.stat.fetch(:idle), a_hash_including('db.pool.name', 'db.namespace')]],
-      'medtracker.db.connection_pool.waiting' => [[pool.stat.fetch(:waiting), a_hash_including('db.pool.name', 'db.namespace')]]
-    )
+    expect(observations).to include(pool_observations(pool))
   end
 
   it 'exports one labelled datapoint for each live pool through the metrics SDK' do
-    primary_config = instance_double(
-      ActiveRecord::DatabaseConfigurations::HashConfig,
-      name: 'primary',
-      database: 'medtracker_test'
+    metric_reader, pools = configured_sdk_metrics
+    metric_reader.pull
+
+    expect(size_data_points(metric_reader)).to contain_exactly(
+      [10, { 'db.pool.name' => 'primary', 'db.namespace' => 'medtracker_test' }],
+      [5, { 'db.pool.name' => 'replica', 'db.namespace' => 'medtracker_replica' }]
     )
-    replica_config = instance_double(
-      ActiveRecord::DatabaseConfigurations::HashConfig,
-      name: 'replica',
-      database: 'medtracker_replica'
+  end
+
+  it 'removes a pool that is no longer live from the next export' do
+    metric_reader, pools = configured_sdk_metrics
+    metric_reader.pull
+
+    pools.replace([pools.last])
+    metric_reader.reset
+    metric_reader.pull
+
+    expect(size_data_points(metric_reader)).to eq(
+      [[5, { 'db.pool.name' => 'replica', 'db.namespace' => 'medtracker_replica' }]]
     )
-    primary_pool = instance_double(
-      ActiveRecord::ConnectionAdapters::ConnectionPool,
-      stat: { size: 10, busy: 4, idle: 6, waiting: 0 },
-      db_config: primary_config,
-      discarded?: false
-    )
-    replica_pool = instance_double(
-      ActiveRecord::ConnectionAdapters::ConnectionPool,
-      stat: { size: 5, busy: 2, idle: 3, waiting: 0 },
-      db_config: replica_config,
-      discarded?: false
-    )
+  end
+
+  def pool_observations(pool)
+    attributes = a_hash_including('db.pool.name', 'db.namespace')
+
+    {
+      'medtracker.db.connection_pool.size' => [[pool.stat.fetch(:size), attributes]],
+      'medtracker.db.connection_pool.in_use' => [[pool.stat.fetch(:busy), attributes]],
+      'medtracker.db.connection_pool.idle' => [[pool.stat.fetch(:idle), attributes]],
+      'medtracker.db.connection_pool.waiting' => [[pool.stat.fetch(:waiting), attributes]]
+    }
+  end
+
+  def configured_sdk_metrics
+    pools = [
+      build_pool('primary', 'medtracker_test', 10, 4, 6),
+      build_pool('replica', 'medtracker_replica', 5, 2, 3)
+    ]
     metric_reader = OpenTelemetry::SDK::Metrics::Export::InMemoryMetricPullExporter.new
     meter_provider = OpenTelemetry::SDK::Metrics::MeterProvider.new
     meter_provider.add_metric_reader(metric_reader)
     meter = meter_provider.meter('medtracker.database_pool.sdk-export-spec')
 
-    pools = [primary_pool, replica_pool]
     Otel::DatabaseConnectionPoolMetrics.new(pool_resolver: -> { pools }, meter:).install
+    [metric_reader, pools]
+  end
 
-    metric_reader.pull
+  def build_pool(name, database, size, busy, idle)
+    db_config = instance_double(ActiveRecord::DatabaseConfigurations::HashConfig, name:, database:)
 
-    size_metric = metric_reader.metric_snapshots.find { |metric| metric.name == 'medtracker.db.connection_pool.size' }
-    expect(size_metric.data_points.map { |point| [point.value, point.attributes] }).to contain_exactly(
-      [10, { 'db.pool.name' => 'primary', 'db.namespace' => 'medtracker_test' }],
-      [5, { 'db.pool.name' => 'replica', 'db.namespace' => 'medtracker_replica' }]
+    instance_double(
+      ActiveRecord::ConnectionAdapters::ConnectionPool,
+      stat: { size:, busy:, idle:, waiting: 0 },
+      db_config:,
+      discarded?: false
     )
+  end
 
-    pools = [replica_pool]
-    metric_reader.reset
-    metric_reader.pull
-
-    size_metric = metric_reader.metric_snapshots.find { |metric| metric.name == 'medtracker.db.connection_pool.size' }
-    expect(size_metric.data_points.map { |point| [point.value, point.attributes] }).to eq(
-      [[5, { 'db.pool.name' => 'replica', 'db.namespace' => 'medtracker_replica' }]]
-    )
+  def size_data_points(metric_reader)
+    metric = metric_reader.metric_snapshots.find do |snapshot|
+      snapshot.name == 'medtracker.db.connection_pool.size'
+    end
+    metric.data_points.map { |point| [point.value, point.attributes] }
   end
 end
