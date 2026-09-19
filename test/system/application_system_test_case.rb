9c6fb494a0ed8f4d1b28ad3cb8fd0d1863265fdc
Dan Webb <dan.webb@damacus.io>
2026-09-19 11:30:30 +0100

    chore(tooling): remove confirmed dead code

--- test/system/application_system_test_case.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :playwright, using: :chromium, screen_size: [1400, 1400]
-end
