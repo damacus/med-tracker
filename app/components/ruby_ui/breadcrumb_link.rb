0e0dce7030326adc6753136fb3881b50ac3cea8a
Dan Webb <dan.webb@damacus.io>
2026-09-19 11:05:38 +0100

    refactor(ui): remove confirmed dead components

--- app/components/ruby_ui/breadcrumb_link.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RubyUI
-  class BreadcrumbLink < Base
-    def view_template(&)
-      a(class: 'transition-colors hover:text-foreground', **attrs, &)
-    end
-  end
-end
